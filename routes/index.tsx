9d8b236cd1db39f9b8ea8d29b88f0e41910a27cb
NaitLee <naitli@foxmail.com>
2023-11-04 17:00:07 +0800

    specify lang and title; better layout for mobile devices

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,11 @@
+import { Head } from "$fresh/runtime.ts";
 import Board from "../islands/Board.tsx";
 
 export default function Home() {
     return <>
+        <Head>
+            <title>工商白板</title>
+        </Head>
         <Board maximize={true} logo="/watermark.svg" css_path="/board.css" />
-        {/* <Board maximize={false} width={640} height={480} /> */}
     </>;
 }
